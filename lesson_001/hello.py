c42fcabbdb3a3ca37f2e13a95e9b235bc2c2fb9a
Muhammad Asif <muasif80@gmail.com>
2023-04-08 06:20:17 +0500

    Hello World example

---
+++ lesson_001/hello.py
@@ -0,0 +1,6 @@
+print("Hello, World")
+# Save this file to any directory directory <DIR>
+# Open terminal and cd to <DIR> 
+# For example if you have saved to your desktop then cd C:\Users\<username>\Desktop
+# Run python hello.py
+# Expected output >> Hello, World
